9f8fa1b35fd564f86bb3c8b8f39ca0a8e7a25432
fajar setianto <muhamad.fajar.setianto@gmail.com>
2021-06-16 23:01:34 +0700

    update

--- database/seeds/QuestionTypeSeeder.php
+++ database/seeds/QuestionTypeSeeder.php
@@ -26,13 +26,13 @@ class QuestionTypeSeeder extends Seeder
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ],[
-            'name' => 'Ganda',
+            'name' => 'Pilihan Ganda',
             'view_build_mode' => '',
             'view_live_mode' => '',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ],[
-            'name' => 'Multiple Choice',
+            'name' => 'Kotak Centang',
             'view_build_mode' => '',
             'view_live_mode' => '',
             'created_at' => date('Y-m-d H:i:s'),
