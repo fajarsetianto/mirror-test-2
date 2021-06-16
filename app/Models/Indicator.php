b27f6c494fa5c009027cae6e10b7858237b76ac1
fajar setianto <muhamad.fajar.setianto@gmail.com>
2021-06-17 01:48:08 +0700

    update

--- app/Models/Indicator.php
+++ app/Models/Indicator.php
@@ -13,30 +13,23 @@ class Indicator extends Model
         return $this->belongsTo(Form::class);
     }
 
-    public function reflection(){
-        return $this->hasOne(Indicator::class,'id','id');
-    }
-
     public function targets(){
         return $this->hasManyThrough(Target::class,Form::class,'id','form_id','form_id','id');
     }
 
     public function targetsWithScore(){
-        return $this->reflection()->with(['targets' => function($q){
-            $q->withAndWhereHas('respondent',function($q){
-                $q->withCount([
-                    'answers as scores' => function($q){
-                            $q->join('offered_answers','offered_answers.id','=','user_answers.offered_answer_id')
-                                ->select(DB::raw('SUM(score) as score'));
-                            }
-                ])
-                ->having('scores', '>', 'indicators.minimum')
-                ->having('scores', '<=', 'indicators.maximum');
-            });
-        }]);
-        
-        
-        // ->targets()
+        $min = $this->attributes['minimum'];
+        $max = $this->attributes['maximum'];
+        return $this->targets()->withAndWhereHas('respondent',function($q) use ($min, $max) {
+                    $q->withCount([
+                        'answers as scores' => function($q){
+                                $q->join('offered_answers','offered_answers.id','=','user_answers.offered_answer_id')
+                                    ->select(DB::raw('SUM(score) as score'));
+                                }
+                    ])
+                    ->having('scores', '>', $min)
+                    ->having('scores', '<=', $max);
+                });
     }
 
     
