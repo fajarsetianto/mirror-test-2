6880961d299f680944b0fab36b33f6ccba4152c3
fajar setianto <muhamad.fajar.setianto@gmail.com>
2021-06-17 02:03:07 +0700

    update

--- app/Models/Indicator.php
+++ app/Models/Indicator.php
@@ -20,14 +20,15 @@ class Indicator extends Model
     public function targetsWithScore(){
         $min = $this->attributes['minimum'];
         $max = $this->attributes['maximum'];
-        return $this->targets()->withAndWhereHas('respondent',function($q) use ($min, $max){
+        $having = 'scores >= '.$min.' and scores >= '.$max;
+        return $this->targets()->withAndWhereHas('respondent',function($q) use ($having){
                     $q->withCount([
                         'answers as scores' => function($q){
                                 $q->join('offered_answers','offered_answers.id','=','user_answers.offered_answer_id')
                                     ->select(DB::raw('SUM(score) as score'));
                                 }
                     ])
-                    ->havingRaw('scores >= '.$min.' and scores >= '.$max);
+                    ->havingRaw($having);
                     // ->havingRaw('scores <= '.$max);
                     // ->having('scores', '>', $this->attributes['minimum'])
                     // ->having('scores', '<=',  $this->attributes['minimum']);
