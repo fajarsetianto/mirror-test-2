fa0e4694db2965412b777c7e45909636ea63987a
hakamanathhar <hakaman99@gmail.com>
2021-06-28 02:44:26 +0700

    fixing question officer

--- resources/views/pages/officer/inspection/do/question/index.blade.php
+++ resources/views/pages/officer/inspection/do/question/index.blade.php
@@ -487,12 +487,12 @@
 	@foreach($officerTarget->target->respondent->answers()->byInstrumentId($item->id)->get() as $key => $row)
 		<script>questionRespondent('{{strtolower($row->question->id)}}','{{strtolower($row->question->questionType->name)}}', '{{$row->question->content}}', '{!!json_encode($row->question->offeredAnswer)!!}', '{!! json_encode($row) !!}')</script>
 	@endforeach
-	@if(count($officerTarget->officer->answers()->get()) < 1)
+	@if(count($officerTarget->answers()->get()) < 1)
 		@foreach($item->questions()->get() as $key => $row)
 			<script>questionOfficer('{{strtolower($row->id)}}','{{strtolower($row->questionType->name)}}', '{{$row->content}}', '{{json_encode($row->offeredAnswer)}}', '{{json_encode($row->officerAnswerOfficer)}}',)</script>
 		@endforeach
 	@else
-		@foreach($officerTarget->officer->answers()->byInstrumentId($item->id)->get() as $key => $row)
+		@foreach($officerTarget->answers()->byInstrumentId($item->id)->get() as $key => $row)
 			<script>questionOfficer('{{strtolower($row->question->id)}}','{{strtolower($row->question->questionType->name)}}', '{{$row->question->content}}', '{!!json_encode($row->question->offeredAnswer)!!}', '{!!json_encode($row)!!}')</script>
 		@endforeach
 	@endif
