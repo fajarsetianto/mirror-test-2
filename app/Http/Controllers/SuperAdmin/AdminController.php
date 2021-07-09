489512270116314dbcd22b141629c25df9b92c4c
fajar setianto <muhamad.fajar.setianto@gmail.com>
2021-07-09 13:38:17 +0700

    update

--- app/Http/Controllers/SuperAdmin/AdminController.php
+++ app/Http/Controllers/SuperAdmin/AdminController.php
@@ -91,7 +91,6 @@ class AdminController extends Controller
                     <div class="dropdown-menu dropdown-menu-right">
                         <a href="#" class="dropdown-item" onclick="component(`'.route('admin.management-user.edit',[$row->id]).'`)"><i class="icon-pencil"></i> Edit</a>
                         <a href="javascript:void(0)" class="dropdown-item" onclick="destroy(`'.route('admin.management-user.destroy',[$row->id]).'`)"><i class="icon-trash"></i> Hapus</a>
-                        <a href="#" class="dropdown-item"><i class="icon-file-word"></i> Export to .doc</a>
                     </div>
                 </div>
             </div>';     
